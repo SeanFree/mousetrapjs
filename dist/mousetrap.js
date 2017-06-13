a48839df7db08d756033c205966846a38b067bf6
sefree <sefree-contractor@cars.com>
2017-06-13 12:13:03 -0400

    Updated handler functions, added states object

--- dist/mousetrap.js
+++ dist/mousetrap.js
@@ -17,18 +17,22 @@ var MouseTrap = function () {
       current: { x: 0, y: 0 },
       last: { x: 0, y: 0 }
     };
-    this.hover = false;
-    this.mousedown = false;
+    this.states = {
+      hover: false,
+      mousedown: false,
+      click: false,
+      dblClick: false,
+      rightClick: false
+    };
     this.debugEnabled = Boolean(debugEnabled);
-    if (this.debugEnabled) this.initDebugConsole();
+    if (this.debugEnabled) this.initDebugPanel();
     this.initEvents();
   }
 
   _createClass(MouseTrap, [{
     key: 'initEvents',
     value: function initEvents() {
-      var prefix = 'mouse';
-      var events = ['enter', 'move', 'over', 'leave', 'out', 'down', 'up'],
+      var events = ['mouseenter', 'mousemove', 'mouseover', 'mouseleave', 'mouseout', 'click', 'dblclick', 'contextmenu', 'mousedown', 'mouseup'],
           self = this;
       var _iteratorNormalCompletion = true;
       var _didIteratorError = false;
@@ -38,7 +42,7 @@ var MouseTrap = function () {
         for (var _iterator = events[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
           var e = _step.value;
 
-          this.element.addEventListener('' + (prefix + e), this.handler.bind(this));
+          this.element.addEventListener(e, this.handler.bind(this));
         }
       } catch (err) {
         _didIteratorError = true;
@@ -58,25 +62,59 @@ var MouseTrap = function () {
   }, {
     key: 'handler',
     value: function handler(e) {
-      this.hover = e.type === 'mousedown' || e.type === 'mouseup' || e.type === 'mouseenter' || e.type === 'mouseover' || e.type === 'mousemove';
-      this.mousedown = e.type === 'mousedown' ? true : e.type === 'mouseup' || e.type === 'mouseleave' || e.type === 'mouseout' ? false : this.mousedown;
       this.position.last.x = this.position.current.x;
       this.position.last.y = this.position.current.y;
       this.position.current.x = e.clientX;
       this.position.current.y = e.clientY;
-      if (this.debugEnabled) this.updateDebugConsole();
+
+      switch (e.type) {
+        case 'mouseenter':
+          this.states.hover = true;
+          break;
+        case 'mousemove':
+          this.states.hover = true;
+          break;
+        case 'mouseover':
+          this.states.hover = true;
+          break;
+        case 'mouseleave':
+          this.states.hover = false;
+          break;
+        case 'mouseout':
+          this.states.hover = false;
+          break;
+        case 'click':
+          this.states.click = true;
+          break;
+        case 'dblclick':
+          this.states.dblClick = true;
+          break;
+        case 'contextmenu':
+          this.states.contextmenu = true;
+          break;
+        case 'mousedown':
+          this.states.mousedown = this.states.hover = true;
+          break;
+        case 'mouseup':
+          this.states.click = this.states.dblClick = this.states.contextmenu = this.states.mousedown = false;
+          break;
+        default:
+          break;
+      }
+      if (this.debugEnabled) this.updateDebugPanel();
     }
   }, {
-    key: 'initDebugConsole',
-    value: function initDebugConsole() {
-      this.debugConsole = document.createElement('div');
-      this.debugConsole.style = '\n      position: fixed;\n      z-index: 2;\n      right: 20px;\n      top: 20px;\n      padding: 10px;\n      background: rgba(80,80,80,0.4);\n      color: white;\n      font-family: sans-serif;\n    ';
-      document.body.appendChild(this.debugConsole);
+    key: 'initDebugPanel',
+    value: function initDebugPanel() {
+      this.debugPanel = document.createElement('div');
+      this.debugPanel.style = '\n      position: fixed;\n      z-index: 2;\n      right: 20px;\n      top: 20px;\n      padding: 10px;\n      background: rgba(80,80,80,0.4);\n      color: white;\n      font-family: sans-serif;\n    ';
+      document.body.appendChild(this.debugPanel);
     }
   }, {
-    key: 'updateDebugConsole',
-    value: function updateDebugConsole() {
-      this.debugConsole.innerHTML = '\n      <div style="padding: 10px" id="current-position-output">\n        <span>Current Position</span><br/>\n        <span style="padding: 5px">x: ' + this.position.current.x + '</span></br>\n        <span style="padding: 5px">y: ' + this.position.current.y + '</span></br>\n      </div>\n      <div style="padding: 10px" id="last-position-output">\n        <span>Last Position</span><br/>\n        <span style="padding: 5px">x: ' + this.position.last.x + '</span></br>\n        <span style="padding: 5px">y: ' + this.position.last.y + '</span></br>\n      </div>\n      <div style="padding: 10px">Mouse Over: ' + this.hover + '</div>\n      <div style="padding: 10px">Mouse Down: ' + this.mousedown + '</div>\n    ';
+    key: 'updateDebugPanel',
+    value: function updateDebugPanel() {
+      console.log(this.states);
+      this.debugPanel.innerHTML = '\n      <div style="padding: 10px" id="current-position-output">\n        <span>Current Position</span><br/>\n        <span style="padding: 5px">x: ' + this.position.current.x + '</span></br>\n        <span style="padding: 5px">y: ' + this.position.current.y + '</span></br>\n      </div>\n      <div style="padding: 10px" id="last-position-output">\n        <span>Last Position</span><br/>\n        <span style="padding: 5px">x: ' + this.position.last.x + '</span></br>\n        <span style="padding: 5px">y: ' + this.position.last.y + '</span></br>\n      </div>\n      <div style="padding: 10px">Mouse Over: ' + this.states.hover + '</div>\n      <div style="padding: 10px">Mouse Down: ' + this.states.mousedown + '</div>\n      <div style="padding: 10px">Double Click: ' + this.states.dblClick + '</div>\n    ';
     }
   }]);
 
--- src/mousetrap.es6
+++ src/mousetrap.es6
@@ -9,38 +9,82 @@ class MouseTrap {
       current: { x: 0, y: 0 },
       last: { x: 0, y: 0 }
     };
-    this.hover = false;
-    this.mousedown = false;
+    this.states = {
+      hover: false,
+      mousedown: false,
+      click: false,
+      dblClick: false,
+      rightClick: false
+    };
     this.debugEnabled = Boolean(debugEnabled);
     if (this.debugEnabled)
-      this.initDebugConsole();
+      this.initDebugPanel();
     this.initEvents();
   }
   initEvents() {
-    const prefix = 'mouse';
-    let events = ['enter', 'move', 'over', 'leave', 'out', 'down', 'up'],
+    let events = [
+        'mouseenter', 
+        'mousemove', 
+        'mouseover', 
+        'mouseleave', 
+        'mouseout', 
+        'click', 
+        'dblclick', 
+        'contextmenu', 
+        'mousedown', 
+        'mouseup'
+      ],
 			self = this;
     for (let e of events) {
-      this.element.addEventListener(`${prefix + e}`, this.handler.bind(this));
+      this.element.addEventListener(e, this.handler.bind(this));
     }
   }
   handler(e) {
-    this.hover = (e.type === 'mousedown' || e.type === 'mouseup' || e.type === 'mouseenter' || e.type === 'mouseover' || e.type === 'mousemove');
-    this.mousedown = (
-      e.type === 'mousedown' ? true :
-      e.type === 'mouseup' || e.type === 'mouseleave' || e.type === 'mouseout' ? false :
-      this.mousedown
-    );
     this.position.last.x = this.position.current.x;
     this.position.last.y = this.position.current.y;
     this.position.current.x = e.clientX;
     this.position.current.y = e.clientY;
+
+    switch (e.type) {
+      case 'mouseenter' :
+        this.states.hover = true;
+        break;
+      case 'mousemove' :
+        this.states.hover = true;
+        break;
+      case 'mouseover' :
+        this.states.hover = true;
+        break;
+      case 'mouseleave' :
+        this.states.hover = false;
+        break;
+      case 'mouseout' :
+        this.states.hover = false;
+        break;
+      case 'click' :
+        this.states.click = true;
+        break;
+      case 'dblclick' :
+        this.states.dblClick = true;
+        break;
+      case 'contextmenu' :
+        this.states.contextmenu = true;
+        break;
+      case 'mousedown' :
+        this.states.mousedown = this.states.hover = true;
+        break;
+      case 'mouseup' :
+        this.states.click = this.states.dblClick = this.states.contextmenu = this.states.mousedown = false;
+        break;
+      default :
+        break;
+    }
     if (this.debugEnabled)
-      this.updateDebugConsole();
+      this.updateDebugPanel();
   }
-  initDebugConsole() {
-    this.debugConsole = document.createElement('div');
-    this.debugConsole.style = `
+  initDebugPanel() {
+    this.debugPanel = document.createElement('div');
+    this.debugPanel.style = `
       position: fixed;
       z-index: 2;
       right: 20px;
@@ -50,10 +94,11 @@ class MouseTrap {
       color: white;
       font-family: sans-serif;
     `;
-    document.body.appendChild(this.debugConsole);
+    document.body.appendChild(this.debugPanel);
   }
-  updateDebugConsole() {
-    this.debugConsole.innerHTML = `
+  updateDebugPanel() {
+    console.log(this.states);
+    this.debugPanel.innerHTML = `
       <div style="padding: 10px" id="current-position-output">
         <span>Current Position</span><br/>
         <span style="padding: 5px">x: ${this.position.current.x}</span></br>
@@ -64,8 +109,9 @@ class MouseTrap {
         <span style="padding: 5px">x: ${this.position.last.x}</span></br>
         <span style="padding: 5px">y: ${this.position.last.y}</span></br>
       </div>
-      <div style="padding: 10px">Mouse Over: ${this.hover}</div>
-      <div style="padding: 10px">Mouse Down: ${this.mousedown}</div>
+      <div style="padding: 10px">Mouse Over: ${this.states.hover}</div>
+      <div style="padding: 10px">Mouse Down: ${this.states.mousedown}</div>
+      <div style="padding: 10px">Double Click: ${this.states.dblClick}</div>
     `;
   }
 }
